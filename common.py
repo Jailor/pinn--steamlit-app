0ee65e26d1e260a50e9d5bce7f2588e8641e5655
andreipelle1 <andreipelle1@gmail.com>
2024-06-30 23:30:47 +0300

    requirements

--- common.py
+++ common.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import streamlit as st
+import tensorflow as tf
+import keras
 from keras.models import load_model
 from sklearn.ensemble import RandomForestRegressor
 import time
 import difflib
-
+import keras.backend as K
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
+from sklearn.model_selection import train_test_split, KFold, cross_validate
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import numpy as np
 def pinn_loss(y_true_with_features, y_pred):
     pass
 
@@ -24,6 +34,129 @@ def load_simplified_models():
                             compile=False)  # best_model_phys_only
 
     return standard_model, pinn_model
+
+
+def train_and_evaluate_model(df, target_column, simple=False):
+    # Prepare the data
+    X = df.drop(columns=[target_column])
+    # if simple:
+    #     columns = st.session_state['columns']
+    #     X = df[columns['water_flow'])
+
+
+    y = df[target_column]
+
+    # Split data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Build the model
+    model = Sequential()
+    model.add(Dense(256, input_dim=X_train.shape[1], activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(1, activation='linear'))
+
+    model.compile(optimizer=Adam(), loss='mean_absolute_error',
+                  metrics=[
+                      keras.metrics.RootMeanSquaredError(),
+                      keras.metrics.MeanAbsoluteError()])
+
+    # Callbacks
+    lr_scheduler = ReduceLROnPlateau(factor=0.66, patience=5)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=20)
+    model_checkpoint = ModelCheckpoint('best_classic_trained.keras', monitor='val_loss', save_best_only=True)
+
+    # Train the model
+    history = model.fit(X_train, y_train, validation_split=0.2,
+                        epochs=100, batch_size=256, callbacks=[early_stopping, model_checkpoint, lr_scheduler])
+
+    # Load the best model
+    model = keras.models.load_model('best_classic_trained.keras')
+
+    # Evaluate the model
+    y_pred = model.predict(X_test)
+    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    r2 = r2_score(y_test, y_pred)
+    mean_observed = y_test.mean()
+    cvrmse = (rmse / mean_observed) * 100
+    mae = mean_absolute_error(y_test, y_pred)
+    mape = (mae / mean_observed) * 100
+
+    print(f"Root Mean Squared Error: {rmse}")
+    print(f"R^2 Score: {r2}")
+    print(f"cvRMSE: {cvrmse}%")
+    print(f"Mean Absolute Error: {mae}")
+    print(f"Mean Absolute Percentage Error: {mape}")
+
+    return model
+
+def train_and_evaluate_physics_model(df, target_column, simple=False):
+
+    alpha = 0.089
+    def pinn_loss(y_true_with_features, y_pred):
+        y_true = y_true_with_features[:, 0:1]
+        outlet_water_temp = y_true_with_features[:, 1:2]
+        inlet_temp = y_true_with_features[:, 2:3]
+        water_flow = y_true_with_features[:, 3:4]
+
+        specific_heat_capacity = 4174  # J/kg°C, specific heat capacity of water
+        gallons_to_liters = 3.78541  # 1 gallon = 3.78541 liters
+        fahrenheit_to_celsius = lambda f: (f - 32) * 5.0 / 9.0  # Convert °F to °C
+        joules_to_btu = 0.0009478171
+        flow_rate_liters = water_flow * gallons_to_liters
+        inlet_temp_c = fahrenheit_to_celsius(inlet_temp)
+        outlet_temp_c = fahrenheit_to_celsius(outlet_water_temp)
+        heat_output = water_flow * (outlet_water_temp - inlet_temp) * 0.997 * 8.3077
+        return tf.reduce_mean(tf.abs(y_true - y_pred) + alpha * tf.abs(heat_output - y_pred), axis=-1)
+    # Prepare the data
+
+    X = df.drop(columns=[target_column])
+    y = df[target_column]
+
+    columns = st.session_state['columns']
+
+    outlet_water_temp = X.iloc[:, X.columns.get_loc(columns['outlet_water_temp'])].to_numpy().reshape(-1, 1)
+    inlet_temp = X.iloc[:, X.columns.get_loc(columns['inlet_temp'])].to_numpy().reshape(-1, 1)
+    water_flow = X.iloc[:, X.columns.get_loc(columns['water_flow'])].to_numpy().reshape(-1, 1)
+
+    y_np = y.to_numpy().reshape(-1, 1)
+
+    y_with_features = np.concatenate(
+        [y_np, outlet_water_temp, inlet_temp, water_flow], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y_with_features, test_size=0.2, random_state=42)
+
+    model = Sequential()
+    model.add(Dense(256, input_dim=X_train.shape[1], activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(1, activation='linear'))
+
+    model.compile(optimizer=Adam(), loss=pinn_loss, metrics=[pinn_loss])
+
+    # Callbacks
+    lr_scheduler = ReduceLROnPlateau(factor=0.66, patience=5)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=20)
+    model_checkpoint = ModelCheckpoint('best_physics_trained.keras', monitor='val_loss', save_best_only=True)
+
+    history = model.fit(X_train, y_train, validation_split=0.2,
+                        epochs=100, batch_size=64, callbacks=[early_stopping, model_checkpoint, lr_scheduler])
+
+    model.load_weights('best_physics_trained.keras')
+
+    y_pred = model.predict(X_test)
+
+    rmse = mean_squared_error(y_test[:, 0], y_pred, squared=False)
+    r2 = r2_score(y_test[:, 0], y_pred)
+    mean_observed = y_test[:, 0].mean()
+    cvrmse = (rmse / mean_observed) * 100
+    mae = mean_absolute_error(y_test[:, 0], y_pred)
+    mape = (mae * 100) / mean_observed
+
+    print(f"Root Mean Squared Error: {rmse}")
+    print(f"R^2 Score: {r2}")
+    print(f"cvRMSE: {cvrmse}%")
+    print(f"Mean Absolute Error: {mae}")
+    print(f"Mean Absolute Percentage Error: {mape}%")
+
+    return model
 
 
 @st.cache_resource
@@ -95,9 +228,9 @@ def load_data(uploaded_file):
 
 
 @st.cache_data
-def get_feature_importances(initial_df):
-    X = initial_df.drop(columns=['Water Heating Load (Btu)'])
-    y = initial_df['Water Heating Load (Btu)']
+def get_feature_importances(initial_df, heating_load_column = 'Water Heating Load (Btu)'):
+    X = initial_df.drop(columns=[heating_load_column])
+    y = initial_df[heating_load_column]
     model = RandomForestRegressor()
     model.fit(X, y)
     importances = model.feature_importances_
